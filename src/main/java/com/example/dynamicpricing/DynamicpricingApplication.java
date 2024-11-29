7515c94dd8991459da131e5e00d8c80545001e48
Luis Arismendi <luisfelipe.arismendi@gmail.com>
2024-11-29 23:19:13 +0100

    feat: initial commit

---
+++ src/main/java/com/example/dynamicpricing/DynamicpricingApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicpricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicpricingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dynamicpricing/DynamicpricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamicpricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicpricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
